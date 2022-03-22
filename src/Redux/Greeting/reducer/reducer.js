00ff29b85cbd339e429184da9ef79c60584fca51
Taher <ttaherhagui@gmail.com>
2022-03-22 15:05:58 +0100

    configurate greeting reducer.

---
+++ src/Redux/Greeting/reducer/reducer.js
@@ -0,0 +1,28 @@
+import { START, FAILURE, GET } from "../action/action";
+
+const greetingReducer = (state= {}, action)=> {
+  switch (action.type) {
+    case START:
+      return {
+        ...state,
+        loading: true,
+      };
+    case FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      }
+    case GET:
+      return {
+        ...state,
+        loading: false,
+        error: false,
+        ...action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+export default greetingReducer;
