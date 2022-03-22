73f2397b61a42f8211345457fe86ff3ed9677bb3
Taher <ttaherhagui@gmail.com>
2022-03-22 14:39:57 +0100

    configure store.

---
+++ src/Redux/configureStore.js
@@ -0,0 +1,8 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
+const reducer = combineReducers({greeting: greetingReducer});
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+export default store;
